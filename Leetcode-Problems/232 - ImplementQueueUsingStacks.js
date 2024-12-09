0e7119100944adcf630739bfe049f6205a1651df
munaimun <munaimunz@gmail.com>
2024-12-09 11:10:50 +0600

    Solution: Leetcode 232. Implement Queue using Stacks

---
+++ Leetcode-Problems/232 - ImplementQueueUsingStacks.js
@@ -0,0 +1,42 @@
+// Leetcode 232. Implement Queue using Stacks
+
+// Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+// Implement the MyQueue class:
+//     void push(int x) Pushes element x to the back of the queue.
+//     int pop() Removes the element from the front of the queue and returns it.
+//     int peek() Returns the element at the front of the queue.
+//     boolean empty() Returns true if the queue is empty, false otherwise.
+
+class MyQueue {
+  constructor() {
+    this.pushStack = [];
+    this.popStack = [];
+  }
+
+  push(value) {
+    this.pushStack.push(value);
+  }
+
+  pop() {
+    if (!this.popStack.length) {
+      while (this.pushStack.length) {
+        this.popStack.push(this.pushStack.pop());
+      }
+    }
+    return this.popStack.pop();
+  }
+
+  peek() {
+    if (!this.popStack.length) {
+      while (this.pushStack.length) {
+        this.popStack.push(this.pushStack.pop());
+      }
+    }
+    return this.popStack[this.popStack.length - 1];
+  }
+
+  empty() {
+    return !this.pushStack.length && !this.popStack.length;
+  }
+}
